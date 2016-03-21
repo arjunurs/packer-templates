3ebfa2aa944696a0b252b59456844ef271cc1369
Preston Bennes <preston.bennes@gmail.com>
2016-03-20 22:46:15 -0700

    move scripts to top of repo as scripts will be shared between templates

---
+++ scripts/install_virtualbox_guest_additions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Where to mount the ISO
+MNTDIR="/mnt"
+# Where we expect the ISO to be
+VBOXGUESTADDSISO="/tmp/VBoxGuestAdditions.iso"
+
+# Verify the ISO is present
+[ -f "${VBOXGUESTADDSISO}" ]
+# Verify MNTDIR exists, is a directory, and is not currently mounted
+[ -d "${MNTDIR}" ] && ! mountpoint "${MNTDIR}"
+mount -o loop "${VBOXGUESTADDSISO}" "${MNTDIR}"
+
+# Packages needed to compile VBoxGuestAdditions.
+# EPEL is needed for dkms.
+yum -y install \
+  dkms gcc gcc-c++ make nfs-utils perl wget \
+  {kernel,openssl,readline,sqlite,zlib}-devel
+
+# MAKE='gmake -i' is needed to successfully build OpenGL.
+MAKE='/usr/bin/gmake -i' /mnt/VBoxLinuxAdditions.run || true
+
+umount "${MNTDIR}"
