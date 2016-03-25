0e34ba3bcc0583835acbeb2afbdb21f56e4f09dc
Preston Bennes <preston@>
2016-03-24 20:17:45 -0700

    jdk installer script

--- scripts/jdk.sh
+++ scripts/jdk.sh
@@ -7,7 +7,9 @@ set -o pipefail
 [ ${UID} -ne 0 ]
 cd "${HOME}"
 
-URL="http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+#URL="http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+URL="http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.rpm"
+#URL="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.rpm"
 FILENAME="$(basename "${URL}")"
 curl -f -L -H 'Cookie: oraclelicense=accept-securebackup-cookie' "${URL}" > "${FILENAME}"
 rpm -qp "${FILENAME}" -K
