ebfea1f9efda6db6b4bc6a25062774b8e8c05657
Ankur Agarwal <ankur.agarwal@snowflake.net>
2018-09-11 20:50:41 -0700

    Makefile changes and binary version upgrades
    Upgrade jdk, nix, IntelliJ version
    Use java only netbeans installer
    Parameterize file names in c6x64/gui-dev-java.json

--- scripts/install_jdk.sh
+++ scripts/install_jdk.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
 
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
+# RPM GPG keys can be downloaded from http://yum.oracle.com/RPM-GPG-KEY-oracle-ol6
+# see https://yum.oracle.com/faq.html
+[ -f /home/vagrant/RPM-GPG-KEY-oracle-ol6 ]
+sudo rpm --import /home/vagrant/RPM-GPG-KEY-oracle-ol6
 rpm -qp "/home/vagrant/${JDK_FILENAME}" -K
 sudo yum -y remove 'java-*-openj*' || /bin/true
 sudo yum -y install "${JDK_FILENAME}"
--- user_build.sh
+++ user_build.sh
@@ -2,5 +2,7 @@
 
 time PACKER_LOG=1 packer build -on-error=ask -var-file=c6x64/newuser.json c6x64/renametonewuser.json
 
+[ "$?" -ne "0" ] && exit 1
+
 sleep 5
 time PACKER_LOG=1 packer build -on-error=ask -var-file=c6x64/newuser.json c6x64/setupuser.json
